e1b8d38a4c6d1631b70131eca9767ed99992aac5
FawazAyazi04 <syedfawazayazi09@gmail.com>
2026-03-03 00:14:26 +0530

    two_sum

---
+++ two_sum/notes.md
@@ -0,0 +1,36 @@
+Solution 1
+
+Idea = Brute force
+
+Edge Case: Indices should not be same. 
+
+Time Complexity: O(n^2) 
+
+Space Complexity: O(1)
+
+I tried first via brute force and got correct in one go
+
+Solution 2
+
+Idea = Sorting + two Pointer
+
+Edge Case: Indices should not be same. 
+
+Time Complexity: O(n log n) 
+
+Space Complexity: O(n) # created a separate sorted list using its indices
+
+For optiimization I thought of sorting the lsit and then using two pointers get to the desired output b. Initially I got stuck in how to get the indices of the original unsorted list as I was returning the sorted list's indices. 
+
+
+Solution 3
+
+Idea = Hashmap
+
+Edge Case: Indices should not be same. 
+
+Time Complexity: O(n) 
+
+Space Complexity: O(n) # created a hashmap
+
+Final optimization thought was of using hashmap, first took difference of current number from target, then searched the difference value in the hashmap as searching in the hashmap take constant time complexity. 
---
+++ two_sum/two_sum.py
@@ -0,0 +1,38 @@
+# Solution 1 -> Brute Force
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+            
+
+
+# Solution 2 -> Sorting+ two pointer
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pairs = [(i, index) for index,i in enumerate(nums)]
+        num = sorted(pairs)
+        print(num)
+        l = 0
+        r = len(num)-1
+        while l<r:
+            if num[l][0] + num[r][0] > target:
+                r -= 1
+            elif num[l][0] + num[r][0] < target:
+                l += 1
+            else:
+                return [num[l][1],num[r][1]]
+            
+# Solution 3 -> Hashmap
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashset = {}  # value:index  
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashset:
+                return [hashset[diff], i]
+            hashset[n] = i
+        return
